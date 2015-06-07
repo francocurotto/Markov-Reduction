afbf7370ccb59825ef96dd5dc76c7a2d473bcb22
Franco <francocurotto@gmail.com>
2015-06-06 22:23:35 -0300

    upload all code

--- Paper Algorithm/Testing/generateMarkovTest.m
+++
@@ -1,4 +0,0 @@
-addpath("mainFun");
-M = generateMarkov(10);
-sumRows = sum(M');
-disp(sumRows);
--- Paper Algorithm/Testing/generateNCDMCTest.m
+++
@@ -1,6 +0,0 @@
-clc; clear all;
-addpath("mainFun");
-M = generateNCDMC([10 30 60 80], 100);
-imagesc(M);
-%save('M2.mat','M');
-min(min(M))
